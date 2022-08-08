8fc2ab1db814cb072adfc938e5f736194674e432
97siwon <110653581+97siwon@users.noreply.github.com>
2022-08-08 10:10:18 +0900

    Update OperatorExample1.java

--- 02_operator/src/edu/kh/op/ex/OperatorExample1.java
+++ 02_operator/src/edu/kh/op/ex/OperatorExample1.java
@@ -34,7 +34,7 @@ public class OperatorExample1 {
 		// 5 % 3 = 2 (나머지)
 		System.out.printf("%d %% %d = %d \n" , input1, input2, input1 % input2);
 		
-		// printf에서 '%' 문자를 출력하기 위해서는 %% 입력
+		// printf에서 '%' 문자를 출력하기 위해서는 %% 입력.
 		
 		
 	}
