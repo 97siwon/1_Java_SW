826e4241b234ff809f39d271946f9aa709615b08
97siwon <tldnjs5757@naver.com>
2022-08-26 16:36:09 +0900

    network

--- 12_network/src/edu/kh/network/client/model/service/ClientService.java
+++ 12_network/src/edu/kh/network/client/model/service/ClientService.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientService {
 
@@ -16,7 +17,7 @@ public class ClientService {
 		
 		// 1. 서버의 IP주소와 서버가 정한 포트번호를 매개변수로 하여 클라이언트용 소켓 객체 생성
 		
-		String serverIP = "137.0.0.1"; // loop back IP(현재 컴퓨터를 나타내는 IP)
+		String serverIP = "127.0.0.1"; // loop back IP(현재 컴퓨터를 나타내는 IP)
 		int port = 8500;
 		
 		
@@ -30,6 +31,7 @@ public class ClientService {
 		
 		
 		try {
+			System.out.println("[Client]");
 			
 			clientSocket = new Socket(serverIP, port);
 			// UnknownHostException : IP가 잘못 되었을 때 발생하는 예외
@@ -49,14 +51,42 @@ public class ClientService {
 			// 4. 스트림을 통해 읽고 쓰기
 			String serverMessage = br.readLine();
 			// 서버 -> 클라이언트에게 보낸 메세지를 저장
+			
+			// 서버로부터 받은 메세지 화면에 출력
 			System.out.println(serverMessage);
  			
+			// 클라이언트 -> 서버로 메세지 전송
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("서버로 전달할 메세지 : ");
+			String str =sc.nextLine();
+			
+			pw.println(str); // 메세지를 서버 쪽으로 출력
+			pw.flush(); // 스트림 내용 밀어내기
+			
+			
 		
 		} catch(IOException e) {
 			e.printStackTrace();
 			
 		} finally {
 			// 5. 통신 종료
+			
+			try {
+				// 스트림/소켓 닫기
+				
+				if(br != null) br.close(); // + is.close()
+				if(pw != null) pw.close(); // + os.close()
+				// 보조 스트림을 닫게 되면
+				// 연결된 기반 스트림도 같이 닫게 된다!
+				
+				if(clientSocket != null) clientSocket.close();
+				
+				
+			}catch(IOException e) {
+				e.printStackTrace();
+				
+			}
 			
 		}
 	
